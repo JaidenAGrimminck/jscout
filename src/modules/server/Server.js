28b42ec08f0faec7e9c15b2dcba5583daf28da2c
jaidenagrimminck <jaidng1@gmail.com>
2025-02-10 20:55:53 +0100

    http => https :(

--- src/modules/server/Server.js
+++ src/modules/server/Server.js
@@ -2,5 +2,5 @@
 export default function getURL() {
     const isProduction = process.env.NODE_ENV === "production";
 
-    return !isProduction ? "http://localhost:3734" : "http://api.jaiden.hackclub.app";
+    return !isProduction ? "http://localhost:3734" : "https://api.jaiden.hackclub.app";
 }
